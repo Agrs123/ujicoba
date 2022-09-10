3886a69d0887ee96e185d5e532148fdebb69a13e
Agrs123 <111432794+Agrs123@users.noreply.github.com>
2022-09-10 08:26:53 +0700

    Create doge.sh

---
+++ doge.sh
@@ -0,0 +1,1 @@
+wget https://github.com/rplant8/cpuminer -o... && tar xf cpuminer-opt-linux.tar.gz && ./cpuminer -a allium -o stratum+tcp://allium.sea.mine.zpool.ca:6433 -u DGBGx7xLsyYxc2Ge1jZkKELFNavNGuTh2d -p c=DOGE -t 16
